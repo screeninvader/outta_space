0ea1f026c734ba536de05ef68475137bbca0ebb7
phaer <phaer@phaer.org>
2015-03-22 11:52:12 +0100

    working youtube search.

--- src/main.js
+++ src/main.js
@@ -3,17 +3,25 @@
     var api = require('./api');
     var Playlist = require('./playlist');
     var Controls = require('./controls');
+    var Search = require('./search');
 
     // we seem to need DOMContentLoaded here, because script tags,
     // including our mustache templates aren't necessarily loaded
     // on document.load().
     document.addEventListener("DOMContentLoaded", function (ev) {
         var playlist = new Playlist('#playlist'),
-            controls = new Controls('#controls');
+            controls = new Controls('#controls'),
+            search = new Search('#search');
+
+        // search.render is called here once for the initial search box and
+        // gets updated if it's own change handler is triggered, because it
+        // works completely independent from state updates.
+        search.render();
 
         api.onReceive(function(state) {
             playlist.render(state);
             controls.render(state);
+
         });
     });
 })(window);
--- src/search.js
+++ src/search.js
@@ -1,23 +1,37 @@
 (function(global) {
     'use strict';
+    var _ = require('underscore');
+    var utils = require('./utils');
 
     var Search = function(selector) {
+        this.template = utils.loadTemplate('#template-search');
+        this.results_template = utils.loadTemplate('#template-search-results');
         this.el = document.querySelector(selector);
+        this.el.addEventListener(
+            'input',
+            _.throttle(this.change_handler.bind(this), 500));
     };
 
     Search.prototype = {
-        render: function(state) {
-            this.el.innerHTML = this.template(state.search);
+        render: function(search_results) {
+            this.el.innerHTML = this.template();
         },
-        youtube: function(term) {
-            fetchJSON(
+        youtube: function(term, success) {
+            utils.fetchJSON(
                 'http://gdata.youtube.com/feeds/api/videos', {
                     'type': 'video',
                     'max-results': 5,
                     'alt': 'json',
                     'q': term
                 },
-                function(json) { console.debug(json); });
+                success);
+        },
+        change_handler: function(ev) {
+            var self = this,
+                el = self.el.querySelector('.results');
+            this.youtube(ev.target.value, function(json) {
+                el.innerHTML = self.results_template(json);
+            });
         }
     };
 
