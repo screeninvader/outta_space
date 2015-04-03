ff1647a915d47a7632d0b31fac318a9775ff00c5
phaer <phaer@phaer.org>
2015-04-03 19:05:25 +0200

    update default config.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    socketUri: 'ws://10.20.30.40:8080',
-    soundcloudClientId: null
+    socketUri: 'ws://screeninvader.local:8080',
+    soundcloudClientId: '2dcf4e12afd6ed5119741f888260fc85'
 };
