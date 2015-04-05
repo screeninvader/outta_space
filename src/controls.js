ccdde95263df7f14b2da15d99265c36e5a2a9cc4
Jascha Ehrenreich <jascha@jaeh.at>
2015-04-05 08:33:01 +0200

    fixed indentation

--- src/controls.js
+++ src/controls.js
@@ -2,27 +2,27 @@ import {loadTemplate, bindEvent} from './utils';
 import api from './api';
 
 class Controls {
-    constructor(selector) {
-        this.el = document.querySelector(selector);
-        this.template = loadTemplate('#template-controls');
-        this.actions = [
-            'previous', 'play', 'pause', 'next'];
-    }
-    render(state) {
-        this.el.innerHTML = this.template({
-            actions: this.actions,
-            volume: state.sound.volume,
-        });
-        bindEvent(this, 'a.action', 'click', this.clickHandler);
-        bindEvent(this, '#volume', 'change', this.volumeHandler);
-    }
-    clickHandler(ev) {
-        api.player[ev.target.id]();
-    }
+  constructor(selector) {
+    this.el = document.querySelector(selector);
+    this.template = loadTemplate('#template-controls');
+    this.actions = [
+      'previous', 'play', 'pause', 'next'];
+  }
+  render(state) {
+    this.el.innerHTML = this.template({
+      actions: this.actions,
+      volume: state.sound.volume,
+    });
+    bindEvent(this, 'a.action', 'click', this.clickHandler);
+    bindEvent(this, '#volume', 'change', this.volumeHandler);
+  }
+  clickHandler(ev) {
+    api.player[ev.target.id]();
+  }
 
-    volumeHandler(ev) {
-        api.setVolume(ev.target.value);
-    }
+  volumeHandler(ev) {
+    api.setVolume(ev.target.value);
+  }
 }
 
 export default Controls;
--- src/utils.js
+++ src/utils.js
@@ -2,44 +2,44 @@ import _ from 'underscore';
 import querystring from 'querystring';
 
 function fetchJSON(url, parameters) {
-    return new Promise((resolve, reject) => {
-        var request = new XMLHttpRequest();
-        url += '?' + querystring.stringify(parameters);
-        request.onload = function() { resolve(this.response); };
-        request.onerror = function(e) { reject(e); };
-        request.open('get', url, true);
-        request.responseType = 'json';
-        request.send();
-    });
+  return new Promise((resolve, reject) => {
+    var request = new XMLHttpRequest();
+    url += '?' + querystring.stringify(parameters);
+    request.onload = function() { resolve(this.response); };
+    request.onerror = function(e) { reject(e); };
+    request.open('get', url, true);
+    request.responseType = 'json';
+    request.send();
+  });
 }
 
 var helpers = {
-    isActive(current, active) {
-        return current + 1 === parseInt(active) ? 'active' : '';
-    },
-    oddEven(index) {
-        return index % 2 ? 'odd' : 'even';
-    },
-    inMinutes(seconds) {
-        return Math.floor(seconds / 60) + ':' + (seconds % 60);
-    }
+  isActive(current, active) {
+    return current + 1 === parseInt(active) ? 'active' : '';
+  },
+  oddEven(index) {
+    return index % 2 ? 'odd' : 'even';
+  },
+  inMinutes(seconds) {
+    return Math.floor(seconds / 60) + ':' + (seconds % 60);
+  }
 };
 
 function loadTemplate(selector) {
-    var el = document.querySelector(selector);
-    return function(context) {
-        return _.template(el.innerHTML)(_.extend(context, helpers));
-    };
+  var el = document.querySelector(selector);
+  return function(context) {
+    return _.template(el.innerHTML)(_.extend(context, helpers));
+  };
 }
 
 function bindEvent(self, target, event, handler) {
-    _.each(self.el.querySelectorAll(target), (el) =>
-           el.addEventListener(event, handler.bind(self), true));
+  _.each(self.el.querySelectorAll(target), (el) =>
+       el.addEventListener(event, handler.bind(self), true));
 }
 
 function bindEvents(self, target, data) {
-    _.each(data, (handler, event) =>
-           bindEvent(self, target, event, handler));
+  _.each(data, (handler, event) =>
+       bindEvent(self, target, event, handler));
 }
 
 export { fetchJSON, loadTemplate, bindEvent, bindEvents };
