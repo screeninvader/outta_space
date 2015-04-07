632c0028c8a52551da13c479a37b4f86ae8f24e9
phaer <phaer@phaer.org>
2015-04-08 00:18:38 +0200

    display a message on connection errors.

--- src/main.js
+++ src/main.js
@@ -12,12 +12,20 @@ document.addEventListener('DOMContentLoaded', ev => {
     , search = new Search('#search')
   ;
 
-  // search.render is called here once for the initial search box and
-  // gets updated if it's own change handler is triggered, because it
-  // works completely independent from state updates.
-  search.render();
+
+  api.onError((error) => {
+    console.error(error);
+    document.querySelector('#message').innerHTML = "Can't connect to server.";
+  });
 
   api.onReceive(state => {
+    // search.render is called here once for the initial search box and
+    // gets updated if it's own change handler is triggered, because it
+    // works completely independent from state updates.
+    _.once(() => {
+      search.render();
+    });
+
     console.debug('Rerendering...');
     playlist.render(state);
     controls.render(state);
