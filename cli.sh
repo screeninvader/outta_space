4cd84a333b9fb597eaea5062be36e269a06eb7aa
Jascha Ehrenreich <jascha@jaeh.at>
2015-04-15 03:29:02 +0200

    added inotify-tools dependency for watch and rebuild

--- cli.sh
+++ cli.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-echo "running jshint"
-jshint \
-  src/ modules/ \
-;
+function build() {
+  echo "running jshint"
+  jshint \
+    src/ \
+  ;
 
-echo 'cli.sh build the themes in node_modules'
-find node_modules/ -type f -name cli.sh \
-  -exec {} build \;\
-;
+  echo 'cli.sh build the themes in node_modules'
+  find node_modules/ -type f -name cli.sh \
+    -exec {} build \;\
+  ;
 
-echo "ln -s ./bundled/default.html ./bundled/index.html"
-cd bundled/
-ln -sf default.html index.html
-cd ../
+  echo "ln -s ./bundled/default.html ./bundled/index.html"
+  cd bundled/
+  ln -sf default.html index.html
+  cd ../
+}
+
+build
+while inotifywait -e close_write src/* node_modules; do build; done
