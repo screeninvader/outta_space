f94c7347b401aba64468e57017da6b020ddc0e57
kallaballa <amir@viel-zu.org>
2015-05-31 16:20:27 +0200

    supplied offical screeninvader youtube api key

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   socketUri: `ws://${window.location.hostname}:8080`
 , soundcloudClientId: '2dcf4e12afd6ed5119741f888260fc85'
-, googleAPIKey: ''
+, googleAPIKey: 'AIzaSyAaWUz_lJrNsdAt4pCW8vtHseLttpTYTyA'
 , searchDebounceWait: 200 // milliseconds (mind the API quotas)
 };
