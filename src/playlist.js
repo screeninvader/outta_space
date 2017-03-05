9762ac2b03acc868b5c5ae269f9a42e394188df8
Bernhard Hayden <hello@nini.su>
2017-03-05 14:37:51 +0100

    Revert "Redesign for playlist item drag'n'dropping"

--- src/playlist.js
+++ src/playlist.js
@@ -17,12 +17,9 @@ class Playlist {
               class="<%= isActive(current, index) %>
                      <%= oddEven(current) %>">
             <div>
-              <span class="grippy">☰</span>
-              <a class="item" href="#<%= current %>">
-                <%= item.title %>
-              </a>
-              <a class="expand">...</a>
+              <a class="item" href="#<%= current %>"><%= item.title %></a>
               <a class="remove">X</a>
+              <a class="expand">...</a>
             </div>
             <div class="expanded">
               <a target="_blank" href="<%= item.source %>">Website</a>
@@ -49,7 +46,7 @@ class Playlist {
     bindEvent(this, '#export', 'click', this.exportHandler);
     var sort = Sortable.create(document.querySelector("#playlist ul"), {
       animation: 0, // ms, animation speed moving items when sorting, `0` — without animation
-      handle: ".grippy", // Restricts sort start click/touch to the specified element
+      handle: "ul", // Restricts sort start click/touch to the specified element
       draggable: "li", // Specifies which items inside the element should be sortable
       onUpdate: function (evt){
         api.playlist.shift(evt.oldIndex, evt.newIndex);
