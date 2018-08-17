cbecd7b3369c7540739fae1b41e2e4fbc7309b11
kallaballa <amir@viel-zu.org>
2018-08-17 10:39:46 +0200

    use location.hostname for websocket connection

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  socketUri: `ws://10.20.30.40:8080`
+  socketUri: 'ws://' + window.location.hostname + ':8080'
 , soundcloudClientId: '2dcf4e12afd6ed5119741f888260fc85'
 , googleAPIKey: 'AIzaSyAaWUz_lJrNsdAt4pCW8vtHseLttpTYTyA'
 , searchDebounceWait: 200 // milliseconds (mind the API quotas)
